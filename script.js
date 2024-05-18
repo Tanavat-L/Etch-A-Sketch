edceae964d2105ba0dc04e3fd362b80ea8a8b195
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-19 01:59:34 +0700

    Add change size button

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
 let container = document.querySelector(".container");
 
-
-for(let i = 0; i<16; i++){
-    let row = document.createElement("div");
-    row.className = "row";
-    row.style.cssText = "display: flex; flex: 1 1 auto;"
-    for(let j = 0; j<16; j++){
-        let column = document.createElement("div");
-        column.className = "column";
-        column.textContent = "a";
-        column.style.cssText = "flex: 1 1 auto; aspect-ratio: 1/1;"
-        row.appendChild(column);
+function makeGrid(sizeX,sizeY){
+    for(let i = 0; i<sizeX; i++){
+        let row = document.createElement("div");
+        row.className = "row";
+        row.style.cssText = "display: flex; flex: 1 1 auto;"
+        for(let j = 0; j<sizeY; j++){
+            let column = document.createElement("div");
+            column.className = "column";
+            column.style.cssText = "flex: 1 1 auto; aspect-ratio: 1/1;"
+            row.appendChild(column);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+makeGrid(16,16);
 
 let columns = document.querySelectorAll(".column");
 
@@ -24,3 +26,13 @@ for(let i = 0; i<columns.length; i++){
         target.style.backgroundColor = "black";
     })
 }
+
+let buttonWrapper = document.createElement("div");
+let firstRow = document.querySelector(".row");
+let button = document.createElement("button");
+button.textContent = "Change grid size";
+button.classText= "grid-size";
+buttonWrapper.appendChild(button);
+container.insertBefore(buttonWrapper,firstRow);
+
+buttonWrapper.style.cssText="display:flex; justify-content: center; align-item: center;"
