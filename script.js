1078a88e029d5157500bc5c65862068a15c11049
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-19 02:36:04 +0700

    Add grid border, add styling, clean up functions

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 
 let container = document.querySelector(".container");
+let currentX = 16;
+let currentY = 16;
 
-function makeGrid(sizeX,sizeY){
-    for(let i = 0; i<sizeY; i++){
+function makeGrid(size){
+    for(let i = 0; i<size; i++){
         let row = document.createElement("div");
         row.className = "row";
         row.style.cssText = "display: flex; flex: 1 1 auto;"
-        for(let j = 0; j<sizeX; j++){
+        for(let j = 0; j<size; j++){
             let column = document.createElement("div");
             column.className = "column";
-            column.style.cssText = "flex: 1 1 auto; aspect-ratio: 1/1;"
+            column.style.cssText = "flex: 1 1 auto; aspect-ratio: 1/1; border: 1px solid black; background-color:white;"
             row.appendChild(column);
         }
         container.appendChild(row);
     }
 }
 
-makeGrid(16,16);
+makeGrid(16);
 addPixelColorChange();
 addSizeChangeButton();
 
@@ -31,21 +33,22 @@ function addPixelColorChange(){
 }
 
 function addSizeChangeButton(){
-let buttonWrapper = document.createElement("div");
-let firstRow = document.querySelector(".row");
-let button = document.createElement("button");
-button.textContent = "Change grid size";
-button.classText= "grid-size";
-buttonWrapper.appendChild(button);
-container.insertBefore(buttonWrapper,firstRow);
-buttonWrapper.style.cssText="display:flex; justify-content: center; align-item: center;"
-button.addEventListener("click", (event) => {
-    let sizeX = +prompt("How many pixel wide? (Max 100px)");
-    let sizeY = +prompt("How many pixel tall? (Max 100px)");
+    let buttonWrapper = document.createElement("div");
+    let firstRow = document.querySelector(".row");
+    let button = document.createElement("button");
+    button.textContent = "Change grid size";
+    button.classText= "grid-size";
+    buttonWrapper.appendChild(button);
+    container.insertBefore(buttonWrapper,firstRow);
+    buttonWrapper.style.cssText="display:flex; justify-content: center; align-item: center; margin: 50px;";
+    buttonWrapper.style.cssText+="font-size: 100px;"
+    button.addEventListener("click", (event) => {
+    let size = +prompt("How many pixel wide/tall? (Max 100px)");
     container.innerHTML = "";
-    makeGrid(sizeX,sizeY);
+    makeGrid(size);
     addPixelColorChange();
     addSizeChangeButton();
+    current = size;
 })
 
 }
