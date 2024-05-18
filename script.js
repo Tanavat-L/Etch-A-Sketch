f398f7ef1595b474902e28888b696e4e0df38c0c
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-19 01:43:37 +0700

    Make grids square

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let container = document.querySelector(".container");
 for(let i = 0; i<16; i++){
     let row = document.createElement("div");
     row.className = "row";
-    row.style.cssText = "display: flex; flex:1 1 0;"
+    row.style.cssText = "display: flex; flex: 1 1 auto;"
     for(let j = 0; j<16; j++){
         let column = document.createElement("div");
         column.className = "column";
         column.textContent = "a";
-        column.style.cssText = "flex: 1 1 0;"
+        column.style.cssText = "flex: 1 1 auto; aspect-ratio: 1/1;"
         row.appendChild(column);
     }
     container.appendChild(row);
 }
 
 let row = document.querySelectorAll(".row");
-container.style.cssText = "display: flex; flex-direction: column;"
+
