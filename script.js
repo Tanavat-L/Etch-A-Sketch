576bb248ad2069147bccd17c44773cb572463da0
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-19 02:16:03 +0700

    Make size change button work

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let container = document.querySelector(".container");
 
 function makeGrid(sizeX,sizeY){
-    for(let i = 0; i<sizeX; i++){
+    for(let i = 0; i<sizeY; i++){
         let row = document.createElement("div");
         row.className = "row";
         row.style.cssText = "display: flex; flex: 1 1 auto;"
-        for(let j = 0; j<sizeY; j++){
+        for(let j = 0; j<sizeX; j++){
             let column = document.createElement("div");
             column.className = "column";
             column.style.cssText = "flex: 1 1 auto; aspect-ratio: 1/1;"
@@ -17,16 +17,20 @@ function makeGrid(sizeX,sizeY){
 }
 
 makeGrid(16,16);
+addPixelColorChange();
+addSizeChangeButton();
 
-let columns = document.querySelectorAll(".column");
-
-for(let i = 0; i<columns.length; i++){
-    columns[i].addEventListener("mouseover", (event) => {
-        let target = event.target;
-        target.style.backgroundColor = "black";
-    })
+function addPixelColorChange(){
+    let columns = document.querySelectorAll(".column");
+    for(let i = 0; i<columns.length; i++){
+        columns[i].addEventListener("mouseover", (event) => {
+            let target = event.target;
+            target.style.backgroundColor = "black";
+        })
+    }
 }
 
+function addSizeChangeButton(){
 let buttonWrapper = document.createElement("div");
 let firstRow = document.querySelector(".row");
 let button = document.createElement("button");
@@ -34,5 +38,14 @@ button.textContent = "Change grid size";
 button.classText= "grid-size";
 buttonWrapper.appendChild(button);
 container.insertBefore(buttonWrapper,firstRow);
-
 buttonWrapper.style.cssText="display:flex; justify-content: center; align-item: center;"
+button.addEventListener("click", (event) => {
+    let sizeX = +prompt("How many pixel wide? (Max 100px)");
+    let sizeY = +prompt("How many pixel tall? (Max 100px)");
+    container.innerHTML = "";
+    makeGrid(sizeX,sizeY);
+    addPixelColorChange();
+    addSizeChangeButton();
+})
+
+}
